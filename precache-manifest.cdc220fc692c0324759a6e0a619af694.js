43ad7f25d8dac117d8fc8ffd82af124a4f1126b9
Andreas_Sujono <andreassujono@gmail.com>
2020-02-17 02:35:00 +0800

    Updates

---
+++ precache-manifest.cdc220fc692c0324759a6e0a619af694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405edc92e1efaa8c1b47dc8ea136742d",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e06089253694e326d681",
+    "url": "/random-quote-machine/static/css/main.f82e6f3c.chunk.css"
+  },
+  {
+    "revision": "af5ab42af7dc6a30e714",
+    "url": "/random-quote-machine/static/js/2.9aca466c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/random-quote-machine/static/js/2.9aca466c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06089253694e326d681",
+    "url": "/random-quote-machine/static/js/main.37279595.chunk.js"
+  },
+  {
+    "revision": "afc9b93c50eb3e83906a",
+    "url": "/random-quote-machine/static/js/runtime-main.daced29d.js"
+  }
+]);
